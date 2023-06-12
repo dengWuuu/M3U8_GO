1bd91a8ddf982cede058d52cd0b50f7996a45744
wudengwu <wudengwu@bytedance.com>
2023-06-12 15:05:51 +0800

    feat: 完成m3u8文件获取，嵌套文件解析

--- tool/m3u8_tool.go
+++ tool/m3u8_tool.go
@@ -3,24 +3,96 @@ package tool
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-func GetM3U8File(url string) []byte {
+const Identify = "#EXTM3U"
+const NestedPrefix = "#EXT-X-STREAM-INF"
+const M3U8Suffix = "m3u8"
+const HTTPPrefix = "http"
+
+func GetM3U8FileContent(url string) ([]string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Println("io.ReadCloser 关闭失败")
-		}
-	}(resp.Body)
+	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Printf("获取m3u8文件内容失败")
+		return nil, err
 	}
-	return body
+
+	lines := strings.Split(string(body), "\n")
+	return lines, nil
+}
+
+func IsM3U8(identify string) bool {
+	return strings.EqualFold(identify, Identify)
+}
+func IsNested(content []string) bool {
+	for _, str := range content {
+		if strings.HasPrefix(str, NestedPrefix) {
+			return true
+		}
+	}
+	return false
+}
+
+func IsSimpleSourceM3U8(content []string) bool {
+	cnt := 0
+	for _, str := range content {
+		if strings.HasSuffix(str, M3U8Suffix) {
+			cnt++
+		}
+	}
+	return cnt <= 1
+}
+
+func GetM3U8IndexURL(url string) string {
+	// 使用 strings 包中的 LastIndex 函数查找最后一个 / 的位置
+	lastIndex := strings.LastIndex(url, "/")
+	// 使用切片操作获取前面部分的 URL
+	indexURL := url[:lastIndex+1]
+	return indexURL
+}
+
+func GetFinalURL(content []string, url string) string {
+	finalURL := ""
+
+	// 理论上m3u8文件地址会在数组后面从后开始遍历更快返回二级URL字符串
+	for i := len(content) - 1; i >= 0; i-- {
+		if strings.HasSuffix(content[i], M3U8Suffix) {
+			// 如果前缀带有 http 说明是完整的 url 不是 uri 不需要拼接
+			if strings.HasPrefix(content[i], HTTPPrefix) {
+				finalURL = content[i]
+				break
+			} else {
+				finalURL = GetM3U8IndexURL(url) + content[i]
+				break
+			}
+		}
+	}
+	return finalURL
+}
+
+func WriteToFile(content []string) {
+
+	// 将字符串切片转换为一个字符串，每个元素以换行符分隔
+	str := ""
+	for _, s := range content {
+		str += s + "\n"
+	}
+
+	// 将字符串写入文件
+	err := ioutil.WriteFile("output.txt", []byte(str), 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+		return
+	}
+
+	fmt.Println("File written successfully")
 }
--- tool/m3u8_tool_test.go
+++ tool/m3u8_tool_test.go
@@ -2,12 +2,54 @@ package tool
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
+func TestURLTool(t *testing.T) {
+	url := "https://hd.lz-cdn18.com/20230610/3269_5e4f3eae/index.m3u8"
+	fmt.Println(strings.EqualFold(GetM3U8IndexURL(url), "https://hd.lz-cdn18.com/20230610/3269_5e4f3eae/"))
+}
+
 func TestGetM3U8(t *testing.T) {
 	url := "https://hd.lz-cdn18.com/20230610/3269_5e4f3eae/index.m3u8"
-	fileByte := GetM3U8File(url)
+	fmt.Printf("读取的URL: %v \n", url)
 
-	fmt.Println(string(fileByte))
+	content, err := GetM3U8FileContent(url)
+
+	if err != nil {
+		fmt.Printf("获取文件内容失败 %v", err)
+		return
+	}
+
+	if !IsM3U8(content[0]) {
+		fmt.Println("不是m3u8文件")
+		return
+	}
+	fmt.Println(content)
+	if !IsNested(content) {
+		fmt.Println("不是嵌套m3u8文件")
+		return
+	}
+
+	if !IsSimpleSourceM3U8(content) {
+		fmt.Println("多源的m3u8文件")
+		return
+	}
+	finalURL := GetFinalURL(content, url)
+	if strings.EqualFold(finalURL, "") {
+		fmt.Println("未获取到嵌套二级URL")
+	}
+	fmt.Println(finalURL)
+
+	content, err = GetM3U8FileContent(finalURL)
+	if err != nil {
+		fmt.Printf("获取嵌套文件内容失败 %v", err)
+		return
+	}
+
+	fmt.Println(content)
+
+	WriteToFile(content)
+
 }
