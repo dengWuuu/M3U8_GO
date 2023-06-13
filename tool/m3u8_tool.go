f887d5fcb1db1315fc712f368346bfbcd014b09e
wudengwu <wudengwu@bytedance.com>
2023-06-13 14:24:51 +0800

    feat: key 生成

--- tool/m3u8_tool.go
+++ tool/m3u8_tool.go
@@ -1,6 +1,8 @@
 package tool
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -97,4 +99,17 @@ func WriteToFile(content []string) {
 	}
 
 	fmt.Println("File written successfully")
+}
+
+func GenerateKey(url string) string {
+	byteURL := []byte(url)
+	hash := sha256.New()
+	//输入数据
+	hash.Write(byteURL)
+	//计算哈希值
+	bytes := hash.Sum(nil)
+	//将字符串编码为16进制格式,返回字符串
+	hashCode := hex.EncodeToString(bytes)
+	//返回哈希值
+	return hashCode
 }
